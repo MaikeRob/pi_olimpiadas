5f0c35aaf4c36dda1505af4560107bb732e3f102
MaikeRob <128054364+MaikeRob@users.noreply.github.com>
2024-05-15 16:04:17 -0300

    Corrigindo caminho de credentials.json

--- models/api_conector/__init__.py
+++ models/api_conector/__init__.py
@@ -6,7 +6,7 @@ api_base_url = 'https://olimpiadasiesb-7780607c931d.herokuapp.com'
 api_login_url = f'{api_base_url}/login/token'
 
 #Carrega as credencias de login
-with open('/home/maike/projetos/pi_olimpiadas/models/api_conector/credentials.json','r') as arquivo:
+with open('models/api_conector/credentials.json','r') as arquivo:
     credentials = json.load(arquivo)
 
 login.autenticator(api_login_url, credentials['username'], credentials['password'])
