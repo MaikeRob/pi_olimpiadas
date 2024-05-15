952baff5e6404ec6c17ee156eb916af1614fd837
MaikeRob <maike.santana2@gmail.com>
2024-05-14 22:17:46 -0300

    modified:   models/api_conector/login.py

--- models/api_conector/login.py
+++ models/api_conector/login.py
@@ -15,7 +15,8 @@ def autenticator(url_login, username, password):
         error = response.json().get('error')
         print(f"Erro no login: {error}")
     elif response.status_code == 403:
-        print("403 : Proibido")
+        error = response.json().get('error')
+        print(f"Proibido : {error}")
     else:
         print("Erro não previsto")
 
