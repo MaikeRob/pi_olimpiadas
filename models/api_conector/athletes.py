fb7c2cc2e36f73680e6d9d4cb1b27f3ac5cf6419
MaikeRob <maike.santana2@gmail.com>
2024-05-26 08:49:32 -0300

    Atualizando e tratando novos erros

--- models/api_conector/athletes.py
+++ models/api_conector/athletes.py
@@ -12,13 +12,15 @@ def getAthletesData():
 
     response = requests.get(api_country_url, headers=headers)
 
-    if response.status_code == 200:
-        athletes_data = response.json()
-        return athletes_data
-    elif response.status_code == 401 or response.status_code == 403:
-        error = response.json().get('error')
-        print(f"{error}\nErro na comunicação com a API...")
-        exit(1)
-    else:
-        print("Erro não previsto")
-        exit(1)
+    match response.status_code:
+        case 200:
+            athletes_data = response.json()
+            return athletes_data
+        case 401 | 403: 
+            error = response.json().get('error')
+            print(f"{error}\nErro na comunicação com a API...")
+            exit(1)
+        case _:
+            print("Erro não previsto")
+            print(f"Mensagem do servidor : {response.text}")
+            exit(1)
--- teste.py
+++ teste.py
@@ -1,7 +1,9 @@
-from requests import get
 from models.api_conector.athletes import getAthletesData
+from models.api_conector.countries import getCountryData
 
-country_data = getAthletesData()
-
+country_data = getCountryData()
 print(country_data)
+atletas_data = getAthletesData()
+
+
 
