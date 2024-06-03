768b7323c36ce398c5da0badf5d442c586b5a29a
MaikeRob <21497970-MaikeRob@users.noreply.replit.com>
2024-06-03 18:12:52 +0000

    Atualizando context_print.py

--- configs/context_print.py
+++ configs/context_print.py
@@ -1,18 +1,18 @@
 #Toda vez que a função print é chamada, imprime quem a chamou 
 import builtins
 import inspect
+from colorama import init, Fore
+
+# Inicializa colorama
+init()
 
 original_print = print
 
 def printWithContext(*args, **kwargs):
-  caller_frame = inspect.stack()[1] #Ultima função? da pilha - função atual
-  caller_module = inspect.getmodule(caller_frame[0]) 
-  caller_function = caller_frame.function 
-  #caller_filename = caller_module.__file__ 
+    caller_frame = inspect.stack()[1] 
+    caller_module = inspect.getmodule(caller_frame[0]) 
+    caller_function = caller_frame.function 
 
-  original_print(f"\033[34m[\033[0m{caller_module}\033[34m]\033[0m\n\033[1;32m{caller_function}\033[0m:",*args,**kwargs)
+    original_print(f"{Fore.BLUE}[{caller_module.__name__}]{Fore.RESET}\n{Fore.GREEN}{caller_function}:{Fore.RESET}", *args, **kwargs)
 
 builtins.print = printWithContext
-
-
-
